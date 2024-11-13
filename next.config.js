3af8b8b8d476b09802fa14638c0fd0ed383088a2
deniprobow <deniprobow@gmail.com>
2024-11-13 08:58:49 +0700

    fix: next config exportoutput

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 
 // https://github.com/vercel/next.js/blob/master/packages/next/next-server/server/config.ts
 const nextConfig = {
-  distDir: 'build',
   webpack: config => {
     const oneOfRule = config.module.rules.find(rule => rule.oneOf);
 
